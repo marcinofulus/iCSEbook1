adb1b063d113775fca0d80e29c0015c67e7ea11a
Severin Wünsch <swuensch@gmail.com>
2016-04-25 16:20:45 +0200

    removed binary string so 'make html' works under python3

--- school/source/conf.py
+++ school/source/conf.py
@@ -37,8 +37,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = b'iCSE for school'
-copyright = b'Marcin Kostur, iCSE-team'
+project = 'iCSE for school'
+copyright = 'Marcin Kostur, iCSE-team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
